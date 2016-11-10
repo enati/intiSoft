c4f1ac255f1d2c769e78724715257d4e6ca07899
enati <natalia.eyherabide@gmail.com>
2016-11-10 10:38:51 -0300

    FIX Cache en produccion

--- intiSoft/settings_prod.py
+++ intiSoft/settings_prod.py
@@ -102,7 +102,7 @@ DATABASES = {
 CACHES = {
    'default': {
       'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-      'LOCATION': '127.0.0.1:80/intiSoft',
+      'LOCATION': '127.0.0.1:80',
    }
 }
 
