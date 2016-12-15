ebb185faad7d9676ebeb40cc87e3df23f7b62fc7
enati <natalia.eyherabide@gmail.com>
2016-12-15 11:25:34 -0300

    MOD Calculo del precio total de ot_linea y ofertatec_linea

--- static/intiSoft/js/base.js
+++ static/intiSoft/js/base.js
@@ -289,33 +289,42 @@ $(document).ready(function() {
         }
     });
 
-    $("[id^=id_ofertatec][id$=cant_horas]").on('change', function (e) {
-        cant_horas_id = $(this).attr('id');
-        formset_number = cant_horas_id.split("-")[1];
-        cant_horas = parseFloat($("[id$="+formset_number+"-cant_horas"+"]").val());
-        precio = parseFloat($("[id$="+formset_number+"-precio"+"]").val());
-        precio_total = $("[id$="+formset_number+"-precio_total"+"]");
-        if (isNaN(cant_horas)) {
-            precio_total.val(precio);
-        }
-        else {
-            precio_total.val(cant_horas*precio);
-        }
+    $("[id^=id_ofertatec][id$=cant_horas], [id^=id_ofertatec][id$=cantidad], [id^=id_ofertatec][id$=precio]")
+        .on('change', function (e) {
+            var cant_horas_id = $(this).attr('id'),
+                formset_number = cant_horas_id.split("-")[1],
+                cantidad = parseFloat($("[id$="+formset_number+"-cantidad"+"]").val()),
+                cant_horas = parseFloat($("[id$="+formset_number+"-cant_horas"+"]").val()),
+                precio = parseFloat($("[id$="+formset_number+"-precio"+"]").val()),
+                precio_total = $("[id$="+formset_number+"-precio_total"+"]"),
+                acc = precio;
+
+                if (!isNaN(cant_horas)) {
+                    acc *= cant_horas;
+                }
+                if (!isNaN(cantidad)) {
+                    acc *= cantidad;
+                }
+                precio_total.val(acc);
     });
 
-    $("[id^=id_adm-ot][id$=cantidad]").on('change', function (e) {
-        cant_horas_id = $(this).attr('id');
-        formset_number = cant_horas_id.split("-")[4];
-        cantidad = parseFloat($("[id$="+formset_number+"-cantidad"+"]").val());
-        cant_horas = parseFloat($("[id$="+formset_number+"-cant_horas"+"]").val());
-        precio = parseFloat($("[id$="+formset_number+"-precio"+"]").val());
-        precio_total = $("[id$="+formset_number+"-precio_total"+"]");
-        if (isNaN(cant_horas)) {
-            precio_total.val(cantidad*precio);
-        }
-        else {
-            precio_total.val(cantidad*cant_horas*precio);
-        }
+    $("[id^=id_adm-ot][id$=cantidad], [id^=id_adm-ot][id$=cant_horas], [id^=id_adm-ot][id$=precio]")
+            .on('change', function (e) {
+                var cant_horas_id = $(this).attr('id'),
+                    formset_number = cant_horas_id.split("-")[4],
+                    cantidad = parseFloat($("[id$="+formset_number+"-cantidad"+"]").val()),
+                    cant_horas = parseFloat($("[id$="+formset_number+"-cant_horas"+"]").val()),
+                    precio = parseFloat($("[id$="+formset_number+"-precio"+"]").val()),
+                    precio_total = $("[id$="+formset_number+"-precio_total"+"]"),
+                    acc = precio;
+
+                if (!isNaN(cant_horas)) {
+                    acc *= cant_horas;
+                }
+                if (!isNaN(cantidad)) {
+                    acc *= cantidad;
+                }
+                precio_total.val(acc);
     });
 
     $("[id^=id_adm-ot][id$=cant_horas]").on('change', function (e) {
