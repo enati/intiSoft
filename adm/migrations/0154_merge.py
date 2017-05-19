5767abd16d11b1eb76d7312a64d3579a4ea2998d
enati <natalia.eyherabide@gmail.com>
2017-05-19 14:06:25 -0300

    Merge migrations

---
+++ adm/migrations/0154_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('adm', '0152_otml_checkbox_sot'),
+        ('adm', '0153_remove_si_fecha_prevista'),
+    ]
+
+    operations = [
+    ]
