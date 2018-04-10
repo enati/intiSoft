aaec6537375785acbb07300faebecaf82a565862
natie <natalia.eyherabide@gmail.com>
2018-04-10 14:10:57 -0300

    [ADD] Usuario logueado en activity log

--- activity_log/models.py
+++ activity_log/models.py
@@ -2,9 +2,11 @@ from __future__ import unicode_literals
 from datetime import datetime
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
+from audit_log.models import AuthStampedModel
 
 
-class ActivityLog(models.Model):
+class ActivityLog(AuthStampedModel):
     datetime = models.DateTimeField("Fecha")
     activity = models.CharField("Actividad", max_length=500)
     comments = models.CharField("Observaciones", max_length=800)
@@ -13,6 +15,11 @@ class ActivityLog(models.Model):
 
     def __str__(self):
         return "%s - %s" % (self.datetime.__format__("%d/%m/%y %H:%M"), self.activity)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.datetime = datetime.now()
+        super(ActivityLog, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['-datetime']
